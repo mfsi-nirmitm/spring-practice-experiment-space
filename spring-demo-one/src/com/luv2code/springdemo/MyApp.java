b13d1ac5eceb04a12ef84461bb371fc0d9bac885
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-12-18 23:11:36 +0530

    dependency injection (Constructor Injection)

--- spring-demo-one/src/com/luv2code/springdemo/MyApp.java
+++ spring-demo-one/src/com/luv2code/springdemo/MyApp.java
@@ -5,11 +5,11 @@ public class MyApp {
 	public static void main(String[] args) {
 		
 		// create the object
-		Coach theCoach = new BaseballCoach();
+		//Coach theCoach = new BaseballCoach();
 		Coach theCoach1 = new TrackCoach();
 		
 		// use the object
-		System.out.println(theCoach.getDailyWorkout());
+		//System.out.println(theCoach.getDailyWorkout());
 		System.out.println(theCoach1.getDailyWorkout());
 
 	}
