c3961003e166ea42f8e5ab6fd4f293030a18d2c0
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-12-23 13:16:03 +0530

    delete query

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/DeleteStudentDemo.java
@@ -20,20 +20,15 @@ public class DeleteStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {			 
-			int studentId = 1;
-						
+			
 			// now get a new session and start transaction
 			session = factory.getCurrentSession();
 			session.beginTransaction();
 			
-			// retrieve student based on the id: primary key
-			System.out.println("\n Getting student with id: " + studentId);
-			
-			Student myStudent = session.get(Student.class, studentId);
-			
-			// delete the student
-			System.out.println("Deleting student: " + myStudent);
-			session.delete(myStudent);
+			// delete the student id = 2
+			System.out.println("Deleting student id = 2" );
+			session.createQuery("delete from Student where id=2")
+				.executeUpdate();
 			
 			// commit the transaction
 			session.getTransaction().commit();
