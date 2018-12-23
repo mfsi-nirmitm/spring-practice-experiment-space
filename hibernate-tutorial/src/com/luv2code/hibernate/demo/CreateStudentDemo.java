cac7f794187d0078fce63c3268acbfe0d8addd68
Nirmit Maheshwari <maheshwari.nirmit24@gmail.com>
2018-12-23 12:54:56 +0530

    queries in hibernate

--- hibernate-tutorial/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
+++ hibernate-tutorial/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -1,5 +1,7 @@
 package com.luv2code.hibernate.demo;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -20,16 +22,39 @@ public class CreateStudentDemo {
 		Session session = factory.getCurrentSession();
 		
 		try {
-			// create a student object
-			System.out.println("Creating new student object...");
-			Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
-			 
-			// start a transaction
+			// get a transaction
 			session.beginTransaction();
 			
-			// save the student object
-			System.out.println("Saving the student...");
-			session.save(tempStudent);
+			// query students
+			List<Student> theStudents = session.createQuery("from Student").getResultList();
+			
+			// display the students
+			displayStudents(theStudents);
+			
+			// query students: lastName='Doe'
+			theStudents = session.createQuery("from Student s where s.lastName='Doe'").getResultList();
+			
+			// display the students
+			System.out.println("\n\nStudents who have last name of Doe");
+			displayStudents(theStudents);
+			
+			// quuery students: lastName='Doe' Or firstname='Daffy'
+			theStudents = 
+					session.createQuery("from Student s where"
+							+ " s.lastName='Doe' OR s.firstName='Daffy'")
+					.getResultList();
+			
+			// display the students
+			System.out.println("\n\nStudent who have last name of Doe or firstName Daffy ");
+			displayStudents(theStudents);
+			
+			// query students where email LIKE '%luv2code.com'
+			theStudents = session.createQuery("from Student s where"
+					+ " s.email LIKE '%luv2code.com'").getResultList();
+			
+			// display the student
+			System.out.println("\n\n students who have email like '%luv2code.com'");
+			displayStudents(theStudents);
 			
 			// commit transaction
 			session.getTransaction().commit();
@@ -40,6 +65,12 @@ public class CreateStudentDemo {
 			factory.close();
 		}
 		
+	}
+
+	private static void displayStudents(List<Student> theStudents) {
+		for (Student tempStudent : theStudents) {
+			System.out.println(tempStudent);
+		}
 	}
 
 }
